22100ef397f75da0d7a752090a5a32dc194d032d
SONG22 <ansongi527@gmail.com>
2021-04-02 11:20:06 +0900

    [setting] 멀티모듈 셋팅

--- market-search/src/main/java/com/pmc/market/SearchApplication.java
+++ market-search/src/main/java/com/pmc/market/SearchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarketApplication {
+public class SearchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MarketApplication.class, args);
+        SpringApplication.run(SearchApplication.class, args);
     }
 
 }
--- market-shop/src/main/java/com/pmc/market/ShopApplication.java
+++ market-shop/src/main/java/com/pmc/market/ShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarketApplication {
+public class ShopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MarketApplication.class, args);
+        SpringApplication.run(ShopApplication.class, args);
     }
 
 }
