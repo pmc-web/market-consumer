674be8a494464b31e46b5583cdcc294c221a2070
SONG22 <ansongi527@gmail.com>
2021-04-05 00:26:41 +0900

    [common][feat] swagger 예제 controller

--- market-common/src/main/java/com/pmc/market/config/SwaggerConfig.java
+++ market-common/src/main/java/com/pmc/market/config/SwaggerConfig.java
@@ -2,19 +2,36 @@ package com.pmc.market.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.tutorialspoint.swaggerdemo")).build();
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.pmc.market"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "PMC Second Project",
+                "Market - Consumer Rest API",
+                "1.0.0",
+                null, null, null, null,
+                new ArrayList<>()
+        );
     }
 }
