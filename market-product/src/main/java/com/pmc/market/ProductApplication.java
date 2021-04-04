c5ab78475d34a4688f14a7c3d45d8adeea191701
kymkyj <ymyj33@naver.com>
2021-04-04 10:11:00 +0900

    [setting] 멀티모듈 기본세팅

--- market-product/src/main/java/com/pmc/market/ProductApplication.java
+++ market-product/src/main/java/com/pmc/market/ProductApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarketApplication {
+public class ProductApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MarketApplication.class, args);
+        SpringApplication.run(ProductApplication.class, args);
     }
 
 }
