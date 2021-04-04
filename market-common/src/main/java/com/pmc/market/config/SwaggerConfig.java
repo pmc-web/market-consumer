073886e70f386acf9188383c44b3b24c72952c95
SONG22 <ansongi527@gmail.com>
2021-04-04 20:41:54 +0900

    [common][feat] swagger 설정

---
+++ market-common/src/main/java/com/pmc/market/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.pmc.market.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.tutorialspoint.swaggerdemo")).build();
+    }
+}
